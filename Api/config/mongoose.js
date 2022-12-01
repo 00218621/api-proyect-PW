1b3e6019e46311063a06e0df1e6c2c2fb4da45cf
00218621 <00218621@uca.edu.sv>
2022-11-30 20:08:58 -0600

    api

--- Api/config/mongoose.js
+++ Api/config/mongoose.js
@@ -1,11 +1,14 @@
 const Mongoose = require("mongoose");
 const debug = require("debug")("app:mongoose");
 
+/*
 const dbhost = process.env.DBHOST || "localhost";
 const dbport = process.env.DBPORT || "27017";
 const dbname = process.env.DBNAME || "wdyt-uca"
 
-const dburi = process.env.DBURI || `mongodb://${dbhost}:${dbport}/${dbname}`;
+*/
+const dburi = process.env.DBURI || `mongodb+srv://drodriguez22:wSNtlodv9Pe7pNwc@clusterelevenpage.2vfjs1z.mongodb.net/dbElevenPage?retryWrites=true&w=majority`;
+
 
 const connect = async () => {
   debug(dburi);
